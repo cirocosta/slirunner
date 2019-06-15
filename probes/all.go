0c84b12c9c315ec60f11f31aab81a97955a16ac8
Ciro S. Costa <cscosta@pivotal.io>
2019-06-15 14:37:40 -0400

    add with-metrics runnable to capture metrics about runs
    Signed-off-by: Ciro S. Costa <cscosta@pivotal.io>

--- probes/all.go
+++ probes/all.go
@@ -13,69 +13,75 @@ func New(target, prefix string) runnable.Runnable {
 	logger.RegisterSink(lager.NewPrettySink(os.Stdout, lager.INFO))
 
 	var createAndRunNewPipeline = runnable.NewWithLogging(logger.Session("create-and-run-new-pipeline"),
-		runnable.NewWithTimeout(
-			runnable.NewShellCommand(FormatProbe(`
+		runnable.NewWithMetrics("create-and-run-new-pipeline",
+			runnable.NewWithTimeout(
+				runnable.NewShellCommand(FormatProbe(`
 
-			set -o errexit
-			set -o xtrace
+				set -o errexit
+				set -o xtrace
 
-			fly -t {{ .Target }} destroy-pipeline -n -p {{ .Pipeline }} || true
-			fly -t {{ .Target }} set-pipeline -n -p {{ .Pipeline }} -c <(echo '`+pipelineContents+`')
-			fly -t {{ .Target }} unpause-pipeline -p {{ .Pipeline }}
+				fly -t {{ .Target }} destroy-pipeline -n -p {{ .Pipeline }} || true
+				fly -t {{ .Target }} set-pipeline -n -p {{ .Pipeline }} -c <(echo '`+pipelineContents+`')
+				fly -t {{ .Target }} unpause-pipeline -p {{ .Pipeline }}
 
-			wait_for_build () {
-				fly -t local builds -j {{ .Pipeline }}/auto-triggering | \
-					grep -v pending | \
-					wc -l
-			}
+				wait_for_build () {
+					fly -t local builds -j {{ .Pipeline }}/auto-triggering | \
+						grep -v pending | \
+						wc -l
+				}
 
-			until [ "$(wait_for_build)" -gt 0 ]; do
-				echo 'waiting for job to automatically trigger...'
-				sleep 1
-			done
+				until [ "$(wait_for_build)" -gt 0 ]; do
+					echo 'waiting for job to automatically trigger...'
+					sleep 1
+				done
 
-			fly -t local watch -j {{ .Pipeline }}/auto-triggering
-			fly -t local destroy-pipeline -n -p {{ .Pipeline }}
+				fly -t local watch -j {{ .Pipeline }}/auto-triggering
+				fly -t local destroy-pipeline -n -p {{ .Pipeline }}
 
-			`, Config{Target: target, Pipeline: prefix + "create-and-run-new-pipeline"}), os.Stderr),
-			60*time.Second,
+				`, Config{Target: target, Pipeline: prefix + "create-and-run-new-pipeline"}), os.Stderr),
+				60*time.Second,
+			),
 		),
 	)
 
 	var hijackFailingBuild = runnable.NewWithLogging(logger.Session("hijack-failing-build"),
-		runnable.NewWithTimeout(
-			runnable.NewShellCommand(FormatProbe(`
+		runnable.NewWithMetrics("hijack-failing-build",
+			runnable.NewWithTimeout(
+				runnable.NewShellCommand(FormatProbe(`
 
-			set -o errexit
-			set -o xtrace
+				set -o errexit
+				set -o xtrace
 
-			fly -t {{ .Target }} set-pipeline -n -p {{ .Pipeline }} -c <(echo '`+pipelineContents+`')
-			fly -t {{ .Target }} unpause-pipeline -p {{ .Pipeline }}
+				fly -t {{ .Target }} set-pipeline -n -p {{ .Pipeline }} -c <(echo '`+pipelineContents+`')
+				fly -t {{ .Target }} unpause-pipeline -p {{ .Pipeline }}
 
-			job_name={{ .Pipeline }}/failing
-			fly -t local trigger-job -j "$job_name" -w || true
+				job_name={{ .Pipeline }}/failing
+				fly -t local trigger-job -j "$job_name" -w || true
 
-			build=$(fly -t local builds -j "$job_name" | head -1 | awk '{print $3}')
-			fly -t local hijack -j "$job_name" -b $build echo Hello World
+				build=$(fly -t local builds -j "$job_name" | head -1 | awk '{print $3}')
+				fly -t local hijack -j "$job_name" -b $build echo Hello World
 
-			`, Config{Target: target, Pipeline: prefix + "hijack-failing-build"}), os.Stderr),
-			60*time.Second,
+				`, Config{Target: target, Pipeline: prefix + "hijack-failing-build"}), os.Stderr),
+				60*time.Second,
+			),
 		),
 	)
 
 	var runExistingPipeline = runnable.NewWithLogging(logger.Session("run-existing-pipeline"),
-		runnable.NewWithTimeout(
-			runnable.NewShellCommand(FormatProbe(`
-			set -o xtrace
-			set -o errexit
+		runnable.NewWithMetrics("run-existing-pipeline",
+			runnable.NewWithTimeout(
+				runnable.NewShellCommand(FormatProbe(`
+				set -o xtrace
+				set -o errexit
 
-			fly -t {{ .Target }} set-pipeline -n -p {{ .Pipeline }} -c <(echo '`+pipelineContents+`')
-			fly -t {{ .Target }} unpause-pipeline -p {{ .Pipeline }}
+				fly -t {{ .Target }} set-pipeline -n -p {{ .Pipeline }} -c <(echo '`+pipelineContents+`')
+				fly -t {{ .Target }} unpause-pipeline -p {{ .Pipeline }}
 
-			fly -t {{ .Target }} trigger-job -w -j "{{ .Pipeline }}/simple-job"
+				fly -t {{ .Target }} trigger-job -w -j "{{ .Pipeline }}/simple-job"
 
-			`, Config{Target: target, Pipeline: prefix + "run-existing-pipeline"}), os.Stderr),
-			60*time.Second,
+				`, Config{Target: target, Pipeline: prefix + "run-existing-pipeline"}), os.Stderr),
+				60*time.Second,
+			),
 		),
 	)
 
---
+++ runnable/duration.go
@@ -0,0 +1,46 @@
+package runnable
+
+import (
+	"context"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+type WithMetrics struct {
+	runnable Runnable
+	name     string
+}
+
+var (
+	_ Runnable = &WithMetrics{}
+
+	runs = promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "slirunner_probe_duration_seconds",
+		Help:    "Time spent running a specific probe",
+		Buckets: []float64{1, 5, 10, 20, 30, 40, 50, 60, 90, 120},
+	}, []string{"probe"})
+)
+
+func NewWithMetrics(probeName string, runnable Runnable) *WithMetrics {
+	return &WithMetrics{
+		runnable: runnable,
+		name:     probeName,
+	}
+}
+
+func (r *WithMetrics) Run(ctx context.Context) (err error) {
+	start := time.Now()
+
+	defer func() {
+		runs.WithLabelValues(r.name).Observe(time.Since(start).Seconds())
+	}()
+
+	err = r.runnable.Run(ctx)
+	if err != nil {
+		return
+	}
+
+	return
+}
